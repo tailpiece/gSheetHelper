9ec67723c76afa2dde724ccf097882e8127f93de
Tail <tail@tailpiece.dev>
2019-08-07 02:47:48 +0900

    update

--- src/background.js
+++ src/background.js
@@ -1,21 +1,101 @@
-var send = null;
-var receive = null;
+let send = null;
+let receive = null;
+let sendWindow = null;
+let receiveWindow = null;
+let sendWindowFullScreen = false;
+let receiveWindowFullScreen = null;
 
 // contentsで送信した値を受信
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         
-        // 送信元からのイベントで、受信先も用意されている場合は、コピペ開始
+        // コピーするための判断情報を、元のタブに送信する
+        sendResponse({sender: sender.tab.id, send: send, receive: receive});
+
+        // 送信元からのイベントで、受信先も用意されている場合は、ペースト 開始
         if ( sender.tab.id === send && receive) {
+
+            // コピー後に、コピー先にフォーカスするかどうか
+            // chrome.tabs.update(receive, {active: true});
+            // chrome.windows.update(receiveWindow, {focused: true});
+
             chrome.tabs.sendMessage(receive, 
-                { text: request.text }
+                { text: request.text, receive: receive, key: false }
             );
         }
-
-        sendResponse({sender: sender.tab.id, send: send, receive: receive});
     }
 );
 
+
+chrome.commands.onCommand.addListener(function(command) {
+    // console.log('onCommand: ', command);
+
+    if ( receive ) {
+
+        let keyCode = 0;
+        switch(command){
+            case "myCommand1": //up
+                keyCode = 38;
+                
+                chrome.tabs.getSelected(window.id, function (tab) {
+                    let mode = false;
+                    if (send && tab.id === send) {
+                        sendWindowFullScreen = !sendWindowFullScreen;
+                        mode = sendWindowFullScreen;
+                    } else if (receive && tab.id === receive) {
+                        receiveWindowFullScreen = !receiveWindowFullScreen;
+                        mode = receiveWindowFullScreen;
+                    } else {
+                        return;
+                    }
+
+                    const stateString = (mode) ? "maximized" : "normal";
+                    chrome.windows.update(tab.windowId, {state: stateString});
+                });
+
+                break;
+
+            case "myCommand2": //down
+                keyCode = 40;
+
+                if ( send && receive) {
+
+                    // コピー
+                    chrome.tabs.sendMessage(send, 
+                        { key: keyCode }
+                    );
+
+                    // コピー後に、コピー先にフォーカスするかどうか
+                    // chrome.tabs.update(receive, {active: true});
+                    // chrome.windows.update(receiveWindow, {focused: true});
+
+                    // ペースト
+                    chrome.tabs.sendMessage(receive, 
+                        { text: "text", receive: receive, key: false }
+                    );
+                }
+
+                break; 
+
+            case "myCommand3": //left
+                keyCode = 37;
+                chrome.tabs.update(send, {active: true});
+                chrome.windows.update(sendWindow, {focused: true});
+                break; 
+
+            case "myCommand4": //right
+                keyCode = 39;
+                chrome.tabs.update(receive, {active: true});
+                chrome.windows.update(receiveWindow, {focused: true});
+                break;
+
+            default:
+                break;
+        }
+
+        
+    }
+});
 
 // 現在の状況に応じてバッジの内容を変える
 const setBadge = function() {
@@ -32,7 +112,7 @@ const setBadge = function() {
     } else {
         chrome.browserAction.setBadgeBackgroundColor({color:[200, 200, 200, 100]});
     }
-    
+
 }
 
 // バッジを初期化
@@ -40,24 +120,30 @@ setBadge();
 
 // アイコンをクリックした際の挙動
 chrome.browserAction.onClicked.addListener(function (e) {
+    // console.info(e);
     const nowID = e.id;
+    const nowWinID = e.windowId;
 
     if(!send && receive !== nowID) {
         send = nowID;
+        sendWindow = nowWinID;
         setBadge();
         return true;
     } else if (send === nowID) {
         send = null;
+        sendWindow = null;
         setBadge();
         return true;
     }
 
     if(!receive) {
         receive = nowID;
+        receiveWindow = nowWinID;
         setBadge();
         return true;
     } else if (receive === nowID) {
         receive = null;
+        receiveWindow = null;
         setBadge();
         return true;
     }
--- src/content_script.js
+++ src/content_script.js
@@ -5,13 +5,6 @@ function onClickEvent(event) {
     // スプレッドシートの、入力欄に表示されている文字列を取得
     const txt = $("#t-formula-bar-input").find(".cell-input").text();
 
-    // クリップボードイベントを上書き（なんかあんまり上手くできていない・・・？）
-    const listener = function(e){
-        e.clipboardData.setData("text/plain" , txt);
-        e.preventDefault();
-        document.removeEventListener("copy", listener);
-    };
-
     // backgroundに送信
     chrome.runtime.sendMessage({ text: txt }, function(res){
         // backgroundから帰ってきた値を確認し、送信元と送信先がセットされていた場合、コピーを行う
@@ -22,20 +15,56 @@ function onClickEvent(event) {
     });
 }
 
+// クリップボードイベントを上書き（なんかあんまり上手くできていない・・・？）
+const listener = function(e){
+    // スプレッドシートの、入力欄に表示されている文字列を取得
+    const txt = $("#t-formula-bar-input").find(".cell-input").text();
+
+    e.clipboardData.setData("text/plain", txt);
+    e.preventDefault();
+    document.removeEventListener("copy", listener);
+};
+
+
+function triggerEvent(element, event) {
+    if (document.createEvent) {
+        // IE以外
+        var evt = document.createEvent("HTMLEvents");
+        evt.initEvent(event, true, true ); // event type, bubbling, cancelable
+        return element.dispatchEvent(evt);
+    } else {
+        // IE
+        var evt = document.createEventObject();
+        return element.fireEvent("on"+event, evt)
+    }
+}
+
+
 // 受信側
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    // テキストの内容があった場合、少し待ってからペースト
-    if(message.text != "") {
-        setTimeout(function(){
-            document.execCommand("paste");
 
-            // イベント発動時に「下矢印キーを押す」の登録
-            var key_event = $.Event('keyup'); 
-            key_event.keyCode = 40;
-            $(document).trigger(key_event);
+    // console.info(message, sender, sendResponse);
+/*
+    // 現在、key_eventがスプレッドシートにtriggerできていないのでコメントアウト
+    var key_event = $.Event('KeyPress'); 
+        key_event.keyCode = (message.key) ? message.key : 40;
+        key_event.which = (message.key) ? message.key : 40;
+*/
+    // console.info( key_event );
 
-        }, 100);
+    if(message.key) {
+        // $(document).trigger(key_event);
+        // triggerEvent(document, key_event);
+        document.execCommand("copy");
     }
 
-    return;
+    // テキストの内容があった場合、少し待ってからペースト
+    if(message.text != "" && !message.key) {
+        // triggerEvent(document, key_event);
+        // $(document).trigger(key_event);
+
+        setTimeout(function(){
+            document.execCommand("paste");
+        }, 200);
+    }
 });
